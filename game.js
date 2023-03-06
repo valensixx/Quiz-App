e4eedc5c43712b822698a8fea248c6f211ec7e14
Valen Black <valensixx@gmail.com>
2023-03-06 19:37:21 +0200

    setting start game and get new questions func

--- game.js
+++ game.js
@@ -44,3 +44,24 @@ let questions = [
 ]
 
 const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+    questionCounter ++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length); 
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+    });
+};
+
+startGame();
