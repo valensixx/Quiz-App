36460405e643f4b67a21f19dc11499f2fec4d0b9
Valen Black <valensixx@gmail.com>
2023-03-07 21:12:34 +0200

    progress bar working fix

--- game.js
+++ game.js
@@ -1,9 +1,8 @@
 const question = document.getElementById('question');
-//const choices = Array.from(document.getElementById('choice-text'));
 const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
-
+const progressBarFull = document.getElementById('progressBarFull');
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -12,60 +11,62 @@ let availableQuestions = [];
 
 let questions = [
     {
-        question: "Where would you be if you were standing on the Spanish Steps?" ,
-        choice1:"Rome",
-        choice2:"Bulgaria",
-        choice3:"China",
-        choice4:"Spain",
+        question: "Where would you be if you were standing on the Spanish Steps?",
+        choice1: "Rome",
+        choice2: "Bulgaria",
+        choice3: "China",
+        choice4: "Spain",
         answer: 1
     },
     {
-        question: "What is the most common surname in the United States?" ,
-        choice1:"Alfred",
-        choice2:"Jhon",
-        choice3:"Smith",
-        choice4:"Neo",
+        question: "What is the most common surname in the United States?",
+        choice1: "Alfred",
+        choice2: "Jhon",
+        choice3: "Smith",
+        choice4: "Neo",
         answer: 3
     },
     {
-        question: "Which planet in the Milky Way is the hottest?" ,
-        choice1:"Mars",
-        choice2:"Venus",
-        choice3:"Earth",
-        choice4:"Uranus",
+        question: "Which planet in the Milky Way is the hottest?",
+        choice1: "Mars",
+        choice2: "Venus",
+        choice3: "Earth",
+        choice4: "Uranus",
         answer: 2
     },
     {
-        question: "How many hearts does an octopus have?" ,
-        choice1:"8",
-        choice2:"1",
-        choice3:"2",
-        choice4:"3",
+        question: "How many hearts does an octopus have?",
+        choice1: "8",
+        choice2: "1",
+        choice3: "2",
+        choice4: "3",
         answer: 4
-    },
-
-]
+    }
+];
 
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
 
-startGame = () => {
+function startGame() {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
     getNewQuestion();
-};
+}
 
-getNewQuestion = () => {
-
-    if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
-        //go to last page
+function getNewQuestion() {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        // go to end page
         return window.location.assign('/end.html');
     }
 
-    questionCounter ++;
+    questionCounter++;
     progressText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length); 
+
+    // update the progress bar
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
 
@@ -76,37 +77,35 @@ getNewQuestion = () => {
 
     availableQuestions.splice(questionIndex, 1);
     acceptingAnswers = true;
-};
+}
 
 choices.forEach(choice => {
-    choice.addEventListener("click", e =>{
-        //console.log(e.target);
-        if(!acceptingAnswers) return;
+    choice.addEventListener('click', e => {
+        if (!acceptingAnswers) return;
 
         acceptingAnswers = false;
-        const selectedChioice = e.target;
-        const selectedAnswer = selectedChioice.dataset["number"];
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
 
         let classToApply = 'incorrect';
-        if(selectedAnswer == currentQuestion.answer){
+        if (selectedAnswer == currentQuestion.answer) {
             classToApply = 'correct';
         }
 
-        if(classToApply ==='correct'){
+        if (classToApply === 'correct') {
             incrementScore(CORRECT_BONUS);
         }
 
-        selectedChioice.parentElement.classList.add(classToApply);
+        selectedChoice.parentElement.classList.add(classToApply);
 
-        setTimeout(()=>{
-            selectedChioice.parentElement.classList.remove(classToApply);
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
             getNewQuestion();
         }, 1000);
-        
     });
 });
 
-incrementScore = num =>{
+function incrementScore(num) {
     score += num;
     scoreText.innerText = score;
 }
