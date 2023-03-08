7cf5eba3c4181d59571314110d4c87adff6f4dc1
Valen Black <valensixx@gmail.com>
2023-03-08 22:00:05 +0200

    end game done for now

--- end.js
+++ end.js
@@ -1,11 +1,15 @@
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+finalScore.innerHTML = mostRecentScore;
+
 username.addEventListener('keyup', () =>{
     //console.log(username.value);
     saveScoreBtn.disabled = !username.value; //if there is no unput in the input field the button will be disabled.
 });
 
 saveHighScore = (e) => {
-    //console.log("clicked the save button!");
-    //e.preventDefault();
+    console.log("clicked the save button!");
+    e.preventDefault();
 };
--- game.js
+++ game.js
@@ -56,6 +56,7 @@ function startGame() {
 
 function getNewQuestion() {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
         // go to end page
         return window.location.assign('/end.html');
     }
