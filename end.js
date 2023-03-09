cac44e5f2b5f77084e07e41e3ccf23e04aed63ec
valensixx <valensixx@gmail.com>
2023-03-09 23:39:34 +0200

    test changes after install windows 10 on PC

--- end.js
+++ end.js
@@ -2,6 +2,8 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+localStorage.setItem('highScores', []);
 finalScore.innerHTML = mostRecentScore;
 
 username.addEventListener('keyup', () =>{
